ef801ce980d74ffa38b9ac47ab41b3a493b9bf07
shiju <shiju@DESKTOP-TNC1U77>
2025-05-20 21:15:41 -0700

    create models

---
+++ Online-food-ordering.Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_food_ordering.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public int SubCategoryID { get; set; }
+        public SubCategory SubCategory { get; set; }
+
+
+    }
+}
