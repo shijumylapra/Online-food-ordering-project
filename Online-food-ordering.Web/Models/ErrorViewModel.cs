3751d19d06a73905278e6b4771f55f90833399e1
shiju <shiju@DESKTOP-TNC1U77>
2025-05-20 20:49:09 -0700

    Add project files.

---
+++ Online-food-ordering.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_food_ordering.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
